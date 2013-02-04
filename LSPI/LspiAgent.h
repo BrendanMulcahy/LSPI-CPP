43d8dbd9ba431e607dc4c660fdadc9eb2aaf4a2b
Tyler Goeringer <tyler.goeringer@gmail.com>
2013-02-03 16:42:45 -0800

    First pass update to template.
    Changed double to float, provided first pass templated implementation of
    public methods. Still need to implement blas.h and update references
    from MatrixOps.

--- LSPI/LspiAgent.h
+++ LSPI/LspiAgent.h
@@ -11,6 +11,7 @@
 #include <vector>
 #include <array>
 #include "MatrixOps.h"
+#include <thrust\host_vector.h>
 
 template <typename vector_type>
 class LspiAgent: public Agent
@@ -29,26 +30,61 @@ class LspiAgent: public Agent
 		 * -v' is the angular velocity after executing the action
 		 * -t is 1 if the state after executing is terminal, 0 otherwise
 		 */
-		LspiAgent(std::vector<std::array<double, 7>> samples, double disc);
+		LspiAgent(thrust::host_vector<float[7]> samples, float discount)
+		{
+			MatrixOps::initializeCUDA();
+			MatrixOps::initializeCUBLAS();
+
+			discount = disc;
+			w = MatrixOps::vec_zeros(BASIS_SIZE*NUM_ACTIONS);
+
+			// Loop until policy converges
+			vector_type policy = lstdq(samples);
+			while(MatrixOps::mag_diff_vec(w, policy) > epsilon_const)
+			{
+				w = policy;
+				policy = lstdq(samples);
+			}
+
+			w = policy;
+		}
 
 		/**
 		 * After creation, the LSPI Agent's policy is used to generate a functional value at a given angle and velocity. This functional value defines the action
 		 * the agent intends to take.
 		 */
-		int getAction(double x, double v);
+		int getAction(float x, float v)
+		{
+			int action = -9999;
+			float max = -9999;
+			int options[3] = {RF_OPT, NF_OPT, LF_OPT};
+			for(int i = 0; i < 3; i++)
+			{
+				vector_type params = basis_function(x, v, options[i]);
+				float q = MatrixOps::dot(params, w);
+				if(q > max)
+				{
+					action = options[i];
+					max = q;
+				}
+			}
+
+			return action;
+		}
 
 	private:
-		double discount;
+		float discount;
 		vector_type w;
 		
 		/**
 		 * Given a set of samples, performs a single update step on the current agent's policy.
 		 */
-		vector_type lstdq(std::vector<std::array<double, 7>> samples);
+		vector_type lstdq(thrust::host_vector<float[7]> samples);
 	
 		/**
 		 * Returns the policy function weights for the given angle, velocity, and action.
 		 * These weights can be used to compute the estimated fitness of the given action.
 		 */
-		vector_type basis_function(double x, double v, int action);
+		vector_type basis_function(float x, float v, int action);
 };
+
